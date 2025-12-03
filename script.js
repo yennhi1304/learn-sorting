85ff39fff4fde0fe865e6ab68f5636b3201f8e16
Yenhi <yenhi130406@gmail.com>
2025-12-03 22:08:48 +0800

    wired buttons

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
 const box = document.getElementById("imageBox");
+const link = document.getElementById("imageLink");
 
 const images = [
     "images/sorting-visualization.gif",
     "images/code-checker.gif",
     "images/games.gif",
-     "images/chatbot.gif",
+    "images/chatbot.gif",
     "images/learning.gif"
+];
+
+const links = [
+    "./sorting_visualizer/index.html",
+    "./sorting_visualizer/index.html",
+    "./sorting_visualizer/index.html",
+    "./sorting_visualizer/index.html",
+    "./theory/index.html",
 ];
 
 let index = 0;
 
-// Start automatic timer
+// Auto timer
 let autoTimer = setInterval(() => {
     changeImage(index + 1);
 }, 8000);
 
-// Function to change image + animate
 function changeImage(newIndex) {
     index = (newIndex + images.length) % images.length;
 
     box.classList.remove("slide");
-    void box.offsetWidth; // reset animation
+    void box.offsetWidth;
     box.classList.add("slide");
 
     box.src = images[index];
+    link.href = links[index];   // <-- updates link dynamically
 }
 
-// Stop auto animation when user interacts
 function stopAuto() {
     clearInterval(autoTimer);
-    autoTimer = null; // optional, prevents restarting
+    autoTimer = null;
 }
 
-// Manual controls
 document.getElementById("prevBtn").onclick = () => {
     stopAuto();
     changeImage(index - 1);
