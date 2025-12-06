3b5bfeadc22a02b408df9cb9305e2b259a1ef73c
yennhi1304 <yenhi130406@gmail.com>
2025-12-07 00:04:38 +0800

    Update editor.js

--- fontend/editor.js
+++ fontend/editor.js
@@ -21,7 +21,7 @@ async function runCode() {
   const code = editor.getValue();
   const lang = document.getElementById("language").value;
 
- const res = await fetch("http://127.0.0.1:8000/run", {
+const res = await fetch("https://code-checker-dbjj.onrender.com/run", {
   method: "POST",
   headers: {"Content-Type": "application/json"},
   body: JSON.stringify({ code, lang })
