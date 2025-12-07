0de77f7ea6a3d9363974792c139e0786a3d5498e
Yenhi <yenhi130406@gmail.com>
2025-12-07 21:16:39 +0800

    change editors

--- frontend/editor.js
+++ frontend/editor.js
@@ -23,7 +23,7 @@ async function runCode() {
   const lang = document.getElementById("language").value;
   const output = document.getElementById("output");
 
-  const res = await fetch("http://127.0.0.1:8000/run", {
+  const res = await fetch("https://code-checker-dbjj.onrender.com/run", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ code, lang })
