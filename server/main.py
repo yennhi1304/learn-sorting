316459592da3301103668dc10acf000327459844
Yenhi <yenhi130406@gmail.com>
2025-12-08 17:42:55 +0800

    change the origin

--- server/main.py
+++ server/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://yennhi1304.github.io"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
