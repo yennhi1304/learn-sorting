6c5275d4709943b249456d892cad22a69615fb49
Yenhi <yenhi130406@gmail.com>
2025-12-08 08:46:47 +0800

    change the files' location

---
+++ frontend/editor.js
@@ -0,0 +1,45 @@
+require.config({ paths: { "vs": "https://cdn.jsdelivr.net/npm/monaco-editor/min/vs" }});
+
+require(["vs/editor/editor.main"], function () {
+
+  window.editor = monaco.editor.create(document.getElementById("editor"), {
+    value: "def my_sort(arr):\n    return arr\n",
+    language: "python",
+    theme: "vs-dark",
+    fontSize: 16,
+  });
+
+  document.getElementById("language").addEventListener("change", e => {
+    const lang = e.target.value;
+    monaco.editor.setModelLanguage(editor.getModel(), lang);
+  });
+});
+
+
+// ------------------------------------------------------------------
+
+async function runCode() {
+  const code = editor.getValue();
+  const lang = document.getElementById("language").value;
+  const output = document.getElementById("output");
+
+  const res = await fetch("https://code-checker-ml8z.onrender.com/run", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ code, lang })
+  });
+
+  const text = await res.text();
+
+  let data;
+  try {
+    data = JSON.parse(text);
+  } catch {
+    output.innerText = "❌ Backend returned non-JSON:\n\n" + text;
+    return;
+  }
+
+  output.innerText = JSON.stringify(data, null, 2);
+}
+
+document.getElementById("runBtn").onclick = runCode;
